a78824e25ded842ce5a8954fc30dbe37fd59b757
yashdev9274 <yashdev.yvd@gmail.com>
2024-05-23 11:34:50 +0530

    feat: create bent-grid for landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Grid } from "@/components/home/grid";
 import Hero from "@/components/home/hero";
 import Navbar from "@/components/home/navbar";
 import Image from "next/image";
@@ -5,12 +6,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <section>
+      <section >
 
         <Navbar />
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
 
           <Hero />
+          <Grid />
         </div>
 
 
